22ddeb16e2ac7ddb8d0decdfebf4cc7a48785a38
mohamed-abdulla <abdabdulla78@gmail.com>
2022-12-24 11:58:55 +0530

    update server link

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server
 
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://codex-nvpt.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
